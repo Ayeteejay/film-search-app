c5633f549e8bb56f109fa35047f8526f079f3ed7
Adam Jones <adam@ayeteejay.com>
2020-10-11 16:55:42 -0700

    Working on fade in/out animations

--- src/App.js
+++ src/App.js
@@ -10,7 +10,10 @@ const App = () => {
     <React.Fragment>
       <div className="site-container">
         <Fade when={!showResults} collapse>
-          <section className="header">
+          <section
+            className="header"
+            style={{ display: !showResults ? "inherit" : "none" }}
+          >
             <h1>OMDb App</h1>
             <p>Looking for the perfect film? Search below. Powered by IMDB.</p>
             <section className="search">
@@ -28,36 +31,62 @@ const App = () => {
           </section>
         </Fade>
         <Fade bottom when={showResults} collapse>
-          <section className="results">
+          <section
+            className="results"
+            style={{ display: showResults ? "inherit" : "none" }}
+          >
             <div className="film-title">
               <h2>Star Wars</h2>
             </div>
             <div className="film-content">
-              <div className="film-description">
-                <div className="film-release">
-                  <h5>Release Date</h5>
-                  <p>October 20, 2020</p>
+              {/* Left Side */}
+              <div className="description">
+                <div className="facts">
+                  <div className="release">
+                    <h5>Release Date</h5>
+                    <p>October 20, 2020</p>
+                  </div>
+                  <div className="rating">
+                    <h5>Rating</h5>
+                    <p>PG</p>
+                  </div>
+                  <div className="runtime">
+                    <h5>Runtime</h5>
+                    <p>152mins</p>
+                  </div>
+                  <div className="genre">
+                    <h5>Genre</h5>
+                    <p>Sci-fi Adventure</p>
+                  </div>
                 </div>
-                <div className="film-rating">
-                  <h5>Rating</h5>
-                  <p>PG</p>
-                </div>
-                <div className="film-runtime">
-                  <h5>Runtime</h5>
-                  <p>152mins</p>
-                </div>
-                <div className="film-genre">
-                  <h5>Genre</h5>
-                  <p>Sci-fi Adventure</p>
+                <div className="details">
+                  <h5>Movie Description</h5>
+                  <p>
+                    It is a long established fact that a reader will be
+                    distracted by the readable content of a page when looking at
+                    its layout. The point of using Lorem Ipsum is that it has a
+                    more-or-less normal distribution of letters, as opposed to
+                    using 'Content here, content here', making it look like
+                    readable English. Many desktop publishing packages and web
+                    page editors now use Lorem Ipsum as their default model
+                    text, and a search for 'lorem ipsum' will uncover many web
+                    sites still in their infancy. Various versions have evolved
+                    over the years, sometimes by accident, sometimes on purpose
+                    (injected humour and the like).
+                  </p>
                 </div>
               </div>
-              <div className="film-poster">
+              {/* Right Side */}
+              <div className="poster">
                 <img src="https://cdn11.bigcommerce.com/s-ydriczk/images/stencil/608x608/products/88997/93196/Avengers-Endgame-Final-Style-Poster-buy-original-movie-posters-at-starstills__42370.1563973510.jpg"></img>
               </div>
             </div>
-            <button onClick={(event) => setShowResults(!showResults)}>
-              Search Again
-            </button>
+            <div className="search-again">
+              <input></input>
+              <button onClick={(event) => setShowResults(!showResults)}>
+                Search Again
+              </button>
+            </div>
           </section>
         </Fade>
       </div>
