03d44d54c4e83a1b36b792e4bd9994cb9ce85ce5
Adam Jones <adam@ayeteejay.com>
2020-10-11 14:17:51 -0700

    Adding in React Reveal

--- src/App.js
+++ src/App.js
@@ -3,15 +3,34 @@ import "./App.scss";
 import Footer from "./components/footer";
 
 const App = () => {
-  const [search, setSearch] = useState("");
+  const [searchValue, setSearchValue] = useState("");
+  const [showResults, setShowResults] = useState(false);
   return (
     <React.Fragment>
       <div className="site-container">
         <section className="header">
           <h1>OMDb App</h1>
-          <p>Looking for the perfect film? Powered by IMDB.</p>
-          <section className="search">
+          <p>Looking for the perfect film? Search below. Powered by IMDB.</p>
+          <section
+            className="search"
+            style={{ display: !showResults ? "flex" : "none" }}
+          >
             <p>Enter your film search below.</p>
+            <input
+              placeholder=""
+              type="text"
+              value={searchValue}
+              onChange={(event) => setSearchValue(event.target.value)}
+            ></input>
+            <button onClick={(event) => setShowResults(!showResults)}>
+              Search
+            </button>
+          </section>
+          <section
+            className="results"
+            style={{ display: showResults ? "flex" : "none" }}
+          >
+            <h2>Star Wars</h2>
           </section>
         </section>
       </div>
