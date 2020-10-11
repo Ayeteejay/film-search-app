98b9236e8c6f59d421e9fd63389282debc967816
Adam Jones <adam@ayeteejay.com>
2020-10-11 13:43:20 -0700

    Creating initial layout

--- src/App.js
+++ src/App.js
@@ -1,26 +1,22 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.scss";
+import Footer from "./components/footer";
 
-function App() {
+const App = () => {
+  const [search, setSearch] = useState("");
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <React.Fragment>
+      <div className="site-container">
+        <section className="header">
+          <h1>OMDb App</h1>
+          <p>Looking for the perfect film? Powered by IMDB.</p>
+          <section className="search">
+            <p>Enter your film search below.</p>
+          </section>
+        </section>
+      </div>
+      <Footer></Footer>
+    </React.Fragment>
   );
-}
-
+};
 export default App;
---
+++ src/components/footer.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <section className="footer">
+      <p>
+        &#169;2020 <a href="https://www.ayeteejay.com/">Ayeteejay</a>.{" "}
+        <span>Be excellent to each other.</span>
+      </p>
+      <p>
+        <a href="https://github.com/ayeteejay">Github</a>
+      </p>
+    </section>
+  );
+};
+export default Footer;
